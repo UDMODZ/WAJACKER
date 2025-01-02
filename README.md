fc7fa138f8ad1f4703eb1b681f3cb6249f249365
UDMODZ <125041148+UDMODZ@users.noreply.github.com>
2025-01-02 21:08:23 +0530

    𝗨𝗗𝗠𝗢𝗗𝗭

---
+++ README.md
@@ -0,0 +1,96 @@
+# WAJacker :- _A NodeJs BASED HACK_
+*A WhatsApp based third party application.*
+
+
+
+<p align="center">
+<img src="https://i.ibb.co/ZX737kS/UD-BOT.png" width="300" height="300"/>
+</p>
+
+<p align="center">
+  <a href="#"><img src="http://readme-typing-svg.herokuapp.com?color=blue&center=true&vCenter=true&multiline=false&lines=Whatsapp+hack+BY+UDMODZ" alt="">
+</p>
+<p align="left">° <a href="https://github.com/UDMODZ/wajacker/edit/main/README.md#support-udmodz-">Stay updated with us</a></p>
+<p align="left">° Dont forget to leave a 🌟 star</p>
+
+
+**Run on Termux**
+
+_Installation/Update_
+
+<p align="left">° Run 1 by one </p>
+
+```
+      $ pkg upgrade -y
+
+      $ pkg update -y
+
+      $ pkg install figlet -y
+
+      $ pkg install wget -y
+
+      $ pkg install ffmpeg
+
+      $ pkg install ffmpeg -y
+
+      $ pkg install imagemagik
+
+      $ pkg install libwebp -y
+
+      $ pkg install nodejs -y
+
+      $ pkg install imagemagik -y
+
+      $ pkg install git
+
+      $ rm -rf wajacker
+
+      $ git clone https://github.com/UDMODZ/wajacker
+
+      $ npm install
+
+```
+
+_Start_
+
+<p align="left">° Run following commands</p>
+
+```
+      $ cd wajacker
+
+      $ npm start
+```
+
+<p align="left">° it will ask for your number.enter your number with international form like 94704638406</p>
+<p align="left">° After that it will give you a Eight digit code.go to whatsapp->three dots-> linked devices->link a new device->link with phone number insted . now type the eight digit code and wait till linking finishes.
+<p align="left">° You did it 😇 type .menu to get menu</p>
+<p align="left">° if you want help contact 
+<a href="https://wa.me/94704638406"> me</a>
+<p align="left"><a href="https://github.com/UDMODZ/wajacker/edit/main/README.md#support-udmodz-">° Stay updated with us</a></p>
+<p align="left">° Dont forget to leave a 🌟 star</p>
+
+
+
+
+
+**Tutorial**      
+
+   comming soon
+
+  
+<h2 align="center">SUPPORT UDMODZ 😘😘😘</h2>
+
+
+
+
+<p align="left">
+<a href="https://www.youtube.com/@UDMODZ">SUBSCRIBE ON YT🥺 </a></p>
+<p align="left">
+<a href="https://t.me/UDMODZ3">FOLLOW   ON TIKTOK🥺</a></p>
+<p align="left">
+<a href="https://whatsapp.com/channel/channel/0029Va5e01M3LdQdtjYJjc3K">FOLLOW   ON WHATSAPP🥺</a></p>
+<p align="left">
+<a href="https://t.me/UDMODZ3">JOIN FOR LRARN HACKING 😎</a></p>
+
+
+
---
+++ udmodz.js
@@ -0,0 +1,69 @@
+
+require("./config")
+const { WA_DEFAULT_EPHEMERAL, getAggregateVotesInPollMessage, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, downloadContentFromMessage, areJidsSameUser, getContentType, useMultiFileAuthState, makeWASocket, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, makeWaSocket } = require("@adiwajshing/baileys")
+const fs = require('fs')
+const util = require('util')
+const axios = require('axios')
+const { exec } = require("child_process")
+const chalk = require('chalk')
+const moment = require('moment-timezone');
+const yts = require ('yt-search');
+const didyoumean = require('didyoumean');
+const readline = require('readline');
+const similarity = require('similarity')
+
+
+
+module.exports = async (udmodz, m) => {
+try {
+const from = m.key.remoteJid
+var body = (m.mtype === 'interactiveResponseMessage') ? JSON.parse(m.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson).id : (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? k.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype == 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ""
+
+const { smsg, fetchJson, getBuffer, fetchBuffer, getGroupAdmins, TelegraPh, isUrl, hitungmundur, sleep, clockString, checkBandwidth, runtime, tanggal, getRandom } = require('./lib/myfunc')
+const { addResponList, delResponList, isAlreadyResponList, isAlreadyResponListGroup, sendResponList, updateResponList, getDataResponList } = require('./lib/respon-list');
+const { isSetProses, addSetProses, removeSetProses, changeSetProses, getTextSetProses } = require('./lib/setproses');
+const { isSetDone, addSetDone, removeSetDone, changeSetDone, getTextSetDone } = require('./lib/setdone');
+
+
+const budy = (typeof m.text === 'string') ? m.text : '';
+const prefixRegex = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/;
+const prefix = prefixRegex.test(body) ? body.match(prefixRegex)[0] : '.';
+const isCmd = body.startsWith(prefix);
+const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : '';
+const args = body.trim().split(/ +/).slice(1)
+const text = q = args.join(" ")
+const sender = m.key.fromMe ? (udmodz.user.id.split(':')[0]+'@s.whatsapp.net' || udmodz.user.id) : (m.key.participant || m.key.remoteJid)
+const botNumber = await udmodz.decodeJid(udmodz.user.id)
+const senderNumber = sender.split('@')[0]
+const isCreator = (m && m.sender && [botNumber, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)) || false;
+const pushname = m.pushName || `${senderNumber}`
+const isBot = botNumber.includes(senderNumber)
+
+
+const quoted = m.quoted ? m.quoted : m
+const mime = (quoted.msg || quoted).mimetype || ''
+const groupMetadata = m.isGroup ? await udmodz.groupMetadata(from).catch(e => {}) : ''
+const groupName = m.isGroup ? groupMetadata.subject : ''
+const participants = m.isGroup ? await groupMetadata.participants : ''
+const groupAdmins = m.isGroup ? await getGroupAdmins(participants) : ''
+const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
+const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+//====================================\\
+let db_respon_list = JSON.parse(fs.readFileSync('./database/list-message.json'));
+let listStore = JSON.parse(fs.readFileSync('./database/list-message.json'));
+let set_proses = JSON.parse(fs.readFileSync('./database/set_proses.json'));
+let set_done = JSON.parse(fs.readFileSync('./database/set_done.json'));
+if (m.message) {
+console.log(chalk.black(chalk.bgWhite('[ New MSG ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> To'), chalk.green(m.isGroup ? pushname : 'Private Chat', from))
+}
+} catch (err) {
+console.log(util.format(err))
+}
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file)
+})
